0304f44ac4a91f3ae169bafca852efb6d1ce24dc
mario.magno <mgmagnoj@gmail.com>
2020-12-13 15:29:12 -0300

    Tarefa2

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -5,19 +5,22 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class BootStrapData implements CommandLineRunner{
-    
+
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;;
 
-
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -39,8 +42,22 @@ public class BootStrapData implements CommandLineRunner{
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
+        Publisher agata = new Publisher();
+        agata.setName("Magno");
+        agata.setLastname("Junior");
+        agata.setAddress("Rua 21 de Abril");
+        agata.setCity("Blumenau");
+        agata.setState("Santa Catarina");
+        agata.setZIPCode("88075000");
+
+        publisherRepository.save(agata);
+
+
         System.out.println("Started Bootstrap");
         System.out.println("Number of Books " + bookRepository.count());
+        System.out.println("My Publisher Count: " + publisherRepository.count());
+
+
 
     }
 }
---
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,117 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String Name;
+    private String Lastname;
+    private String Address;
+    private String City;
+    private String State;
+    private String ZIPCode;
+
+
+    public Publisher() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", Name='" + getName() + "'" +
+            ", Lastname='" + getLastname() + "'" +
+            ", Address='" + getAddress() + "'" +
+            ", City='" + getCity() + "'" +
+            ", State='" + getState() + "'" +
+            ", ZIPCode='" + getZIPCode() + "'" +
+            "}";
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Publisher)) {
+            return false;
+        }
+        Publisher publisher = (Publisher) o;
+        return id == publisher.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getLastname() {
+        return this.Lastname;
+    }
+
+    public void setLastname(String Lastname) {
+        this.Lastname = Lastname;
+    }
+
+    public String getAddress() {
+        return this.Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public String getCity() {
+        return this.City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return this.State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZIPCode() {
+        return this.ZIPCode;
+    }
+
+    public void setZIPCode(String ZIPCode) {
+        this.ZIPCode = ZIPCode;
+    }
+
+
+    
+}
